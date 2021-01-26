c262f45c48035284bef299674c3543a98529995c
andry <andry@DESKTOP-SENM7HG>
2021-01-26 15:12:18 +0100

    Add project files.

---
+++ GestioneArray/Gestione.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestioneArray
+{
+    public class Gestione
+    {
+        private static int[] array = new int[10];
+
+        public static void Riempimento(bool scelta)
+        {
+            if(scelta == true)
+            {
+                Random r = new Random();
+                for(int c = 0; c < array.Length;c++)
+                {
+                    array[c] = r.Next();
+                }
+            }
+            else
+            {
+                int c;
+                for (c = 0; c < array.Length;c++)
+                {
+                    Console.Write($"Inserisci il {c + 1}° numero nella posizione {c}: ");
+                    array[c] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+        }
+
+        public static void Stampa(bool scelta1)
+        {
+            if(scelta1 == true)
+            {
+                for (int c = 0; c < array.Length; c++)
+                    Console.Write($"{array[c]} ");
+            }
+            else
+            {
+                for (int c = array.Length - 1; c >= 0; c--)
+                    Console.Write($"{array[c]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ ProvaArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using GestioneArray;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Se vuoi riempire l'array casualmente digita 1, Se invece lo vuoi riempire manualmente digita 2: ");
+            bool riempi = Console.ReadLine() == "1";
+            Gestione.Riempimento(riempi);
+            Console.Write("Se vuoi visualizzare l'array da destra verso sinistra digita 1, se invece lo vuoi vedere da sinistra verso destra digita 2: ");
+            bool stampa = Console.ReadLine() == "1";
+            Gestione.Stampa(stampa);
+            Console.ReadLine();
+
+        }
+    }
+}
